2be18d925ebe8d7a109d9614bf9cdcbce9da3480
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-17 19:08:45 -0700

    Remove bundle for now and complete preliminary pixel processing

--- build/bundle.js
+++ build/bundle.js
@@ -1,19 +1,84 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-module.exports = function () {
-  return "contruction...";
-};
+// state
+const colorMap = new Map();
+let currentStage = "image"; // "histogram"
+let ctx;
+let highestBinCount = 0;
+let width;
+let height;
+let urlInputValue;
 
-},{}],2:[function(require,module,exports){
-module.exports = function () {
-  return "Under ";
-};
+const img = new Image();
+img.crossOrigin = "Anonymous";
 
-},{}],3:[function(require,module,exports){
-const foo = require("./lib/foo");
-const bar = require("./lib/bar");
 
-// const elem = document.querySelector("#construction");
-const x = foo() + bar();
-// elem.textContent = x;
+// 1. handle local file upload
+const inputTypeFile = document.getElementById("local-file");
+inputTypeFile.addEventListener("change", handleLocalFile);
 
-},{"./lib/bar":1,"./lib/foo":2}]},{},[3]);
+function handleLocalFile(e) {
+  const file = e.currentTarget.files[0];
+  img.src = window.URL.createObjectURL(file);
+}
+
+// 2. handle image url input
+const urlForm = document.getElementById("image-link-form");
+urlForm.addEventListener("submit", handleUrlInput);
+const urlInput = document.getElementById("image-link");
+urlInput.addEventListener("change", handleUrlInputChange)
+
+function handleUrlInput(e) {
+  e.preventDefault();
+  img.src = urlInputValue;
+}
+function handleUrlInputChange(e) {
+  urlInputValue = e.currentTarget.value;
+}
+
+// 3. When image is loaded, do this
+const canvas = document.getElementById("canvas");
+img.addEventListener("load", onImageLoad);
+
+function onImageLoad() {
+  width = img.width;
+  height = img.height;
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  // get pixel data
+  const imgData = ctx.getImageData(0, 0, width, height);
+  const pixels = imgData.data;
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    const r = pixels[i];
+    const g = pixels[i + 1];
+    const b = pixels[i + 2];
+    const color = chroma(r, g, b);
+    const lightness = color.get("hsl.l");
+    let pixelInfo = colorMap.get(lightness);
+    if (!pixelInfo) {
+      pixelInfo = { points: [] };
+      colorMap.set(lightness, pixelInfo);
+    }
+    pixelInfo.points.push({
+      x: Math.floor(i / 1200),
+      y: Math.floor(i % 1200 / 4),
+      duration: Math.round(Math.random() * 120) + 30,
+      frame: 0,
+      destX: 0,
+      destY: 0,
+      color,
+    });
+
+    // find the highest bin
+    if (pixelInfo.points.length > highestBinCount)
+      highestBinCount = pixelInfo.points.length;
+  }
+
+  console.log(colorMap);
+}
+
+},{}]},{},[1]);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,80 @@
-const foo = require("./lib/foo");
-const bar = require("./lib/bar");
+// state
+const colorMap = new Map();
+let currentStage = "image"; // "histogram"
+let ctx;
+let highestBinCount = 0;
+let width;
+let height;
+let urlInputValue;
 
-// const elem = document.querySelector("#construction");
-const x = foo() + bar();
-// elem.textContent = x;
+const img = new Image();
+img.crossOrigin = "Anonymous";
+
+
+// 1. handle local file upload
+const inputTypeFile = document.getElementById("local-file");
+inputTypeFile.addEventListener("change", handleLocalFile);
+
+function handleLocalFile(e) {
+  const file = e.currentTarget.files[0];
+  img.src = window.URL.createObjectURL(file);
+}
+
+// 2. handle image url input
+const urlForm = document.getElementById("image-link-form");
+urlForm.addEventListener("submit", handleUrlInput);
+const urlInput = document.getElementById("image-link");
+urlInput.addEventListener("change", handleUrlInputChange)
+
+function handleUrlInput(e) {
+  e.preventDefault();
+  img.src = urlInputValue;
+}
+function handleUrlInputChange(e) {
+  urlInputValue = e.currentTarget.value;
+}
+
+// 3. When image is loaded, do this
+const canvas = document.getElementById("canvas");
+img.addEventListener("load", onImageLoad);
+
+function onImageLoad() {
+  width = img.width;
+  height = img.height;
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  // get pixel data
+  const imgData = ctx.getImageData(0, 0, width, height);
+  const pixels = imgData.data;
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    const r = pixels[i];
+    const g = pixels[i + 1];
+    const b = pixels[i + 2];
+    const color = chroma(r, g, b);
+    const lightness = color.get("hsl.l");
+    let pixelInfo = colorMap.get(lightness);
+    if (!pixelInfo) {
+      pixelInfo = [];
+      colorMap.set(lightness, pixelInfo);
+    }
+    pixelInfo.push({
+      x: Math.floor(i / 1200),
+      y: Math.floor((i % 1200) / 4),
+      duration: Math.round(Math.random() * 120) + 30,
+      frame: 0,
+      destX: 0,
+      destY: 0,
+      color,
+    });
+
+    // find the highest bin
+    if (pixelInfo.length > highestBinCount) highestBinCount = pixelInfo.length;
+  }
+
+  console.log(colorMap);
+}
