2cb27c3dab62f247d1fb5a02cc09338e7fc047c4
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-23 01:34:05 -0700

    Add play/pause button entering animation

--- src/main.js
+++ src/main.js
@@ -9,21 +9,18 @@ function toggleOptions() {
   if (topSection.classList.contains("collapsed")) {
     optionsButton.innerHTML = "<i class='fas fa-sliders-h'></i> Options";
   } else {
-    optionsButton.innerHTML = "<i class='fas fa-sliders-h'></i> Hide Options";
+    optionsButton.innerHTML = "<i class='fas fa-sliders-h'></i> Hide";
   }
 }
 
 
 /* ----- Various input types for the image: ----- */
-/*
-1) local file
-2) url input
-3) demo
-4) apply with new options
-*/
-
 const img = new Image();
 img.crossOrigin = "Anonymous";
+// 1) local file
+// 2) url input
+// 3) demo
+// 4) apply with new options
 
 // 1. handle local file upload
 const inputTypeFile = document.getElementById("local-file");
@@ -55,21 +52,34 @@ function handleUrlInput(e) {
 }
 
 // 3. handle random image from demo button click
-const listOfDemos = require("./lib/demo_urls");
+// const listOfDemos = require("./lib/demo_urls");
+const listOfDemos = [
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Vincent_van_Gogh_-_Almond_blossom_-_Google_Art_Project.jpg/300px-Vincent_van_Gogh_-_Almond_blossom_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Van_Gogh_-_Trauernder_alter_Mann.jpeg/300px-Van_Gogh_-_Trauernder_alter_Mann.jpeg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Irises-Vincent_van_Gogh.jpg/300px-Irises-Vincent_van_Gogh.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Van_Gogh_-_Terrasse_des_Caf%C3%A9s_an_der_Place_du_Forum_in_Arles_am_Abend1.jpeg/300px-Van_Gogh_-_Terrasse_des_Caf%C3%A9s_an_der_Place_du_Forum_in_Arles_am_Abend1.jpeg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/300px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/220px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Vincent_van_Gogh_-_Sunflowers_-_VGM_F458.jpg/240px-Vincent_van_Gogh_-_Sunflowers_-_VGM_F458.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Vincent_Van_Gogh_-_Wheatfield_with_Crows.jpg/300px-Vincent_Van_Gogh_-_Wheatfield_with_Crows.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg/300px-Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/De_zaaier_-_s0029V1962_-_Van_Gogh_Museum.jpg/200px-De_zaaier_-_s0029V1962_-_Van_Gogh_Museum.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg",
+];
 const demoButton = document.querySelector(".demo-button");
 demoButton.addEventListener("click", onDemoClick);
 
 let lastDemoIdx;
+let chosenDemoIdx;
 function onDemoClick() {
   clearOldAnimation();
   if (!topSection.classList.contains("collapsed")) toggleOptions();
 
-  let chosenIdx;
-  while (chosenIdx === lastDemoIdx) {
-    chosenIdx = Math.floor(Math.random() * (listOfDemos.length - 1));
+  while (chosenDemoIdx === lastDemoIdx) {
+    chosenDemoIdx = Math.floor(Math.random() * (listOfDemos.length - 1));
   }
-  lastDemoIdx = chosenIdx;
-  const chosenUrl = listOfDemos[chosenIdx];
+  lastDemoIdx = chosenDemoIdx;
+  const chosenUrl = listOfDemos[chosenDemoIdx];
   urlInput.value = chosenUrl;
   img.src = chosenUrl;
 }
@@ -78,47 +88,41 @@ function onDemoClick() {
 function handleApplyChanges() {
   clearOldAnimation();
   toggleOptions();
-
+  // fixme
 }
 
 /* ----- All necessary cancelAnimationFrame()'s ----- */
 function clearOldAnimation() {
-
+  // fixme
 }
+
+/* ----- Adjustable settings related ----- */
+let numPixelsConstant; // fixme
+let numBucketsConstant;
+let numFramesConstant = 30;
+let fillStyleConstant = "#13294f";
 
 /* ----- When image is loaded, do this ----- */
 img.addEventListener("load", onImageLoad);
+const canvas = document.getElementById("canvas");
 
 // state
-const canvas = document.getElementById("canvas");
+let firstStart = true;
 let width;
 let height;
 let ctx;
 const colorMap = new Map();
-let highestBinCount = 0;
-
-let currentStageIsImage = true; // true = image, false = histogram
-
 let aniReq1;
 
-let aniReq2;
-let aniReq3;
-
-let firstStart = true;
-
 function onImageLoad() {
-  if (firstStart) {
-    // Reveal play/pause button
-    document.querySelector(".top-menu").classList.add("show-play");
-  } else {
-    resetAll();
-  }
+  if (firstStart) showPlayButton();
   firstStart = false;
 
-  width = img.width;
-  height = img.height;
-  canvas.width = width;
-  canvas.height = height;
+  width = canvas.width = img.width;
+  height = canvas.height = img.height;
+
+  addImgDimensionsToUI(width, height);
+  animateProgBar(width * height);
 
   ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
@@ -127,6 +131,7 @@ function onImageLoad() {
   const imgData = ctx.getImageData(0, 0, width, height);
   const pixels = imgData.data;
 
+  let highestBinCount = 0;
   for (let i = 0; i < pixels.length; i += 4) {
     const r = pixels[i];
     const g = pixels[i + 1];
@@ -134,31 +139,33 @@ function onImageLoad() {
     const color = chroma(r, g, b);
     const lightness = color.get("hsl.l");
     let pixelInfo = colorMap.get(lightness);
+
     if (!pixelInfo) {
       pixelInfo = [];
       colorMap.set(lightness, pixelInfo);
     }
+
     pixelInfo.push({
       x: Math.floor((i % (width * 4)) / 4),
       y: Math.floor(i / (width * 4)),
-      duration: Math.round(Math.random() * 120) + 30,
-      frame: 0,
+      totalNumFrames: Math.round(Math.random() * 120) + 30,
+      currentFrameIdx: 0,
       destX: 0,
       destY: 0,
       color,
     });
 
-    // find the highest bin
     if (pixelInfo.length > highestBinCount) highestBinCount = pixelInfo.length;
   }
 
+  console.log(colorMap);
   // calculate destX and destY for everything in colorMap
   colorMap.forEach((arr, key) => {
     const xOffset = key * width;
 
     arr.forEach((point, idx) => {
       point.destX = xOffset;
-      point.destY = height * 0.95 - (0.95 * height * idx) / highestBinCount;
+      point.destY = height - (height * idx) / highestBinCount;
     });
   });
 
@@ -167,26 +174,29 @@ function onImageLoad() {
   }, 1000);
 }
 
+let currentStageIsImage = true; // true = image, false = histogram
+let aniReq2;
+let aniReq3;
+
 function draw() {
-  let hasMore = false;
-
-  ctx.fillStyle = "#13294f";
+  ctx.fillStyle = fillStyleConstant;
   ctx.fillRect(0, 0, width, height);
-
+  
   const imgData = ctx.getImageData(0, 0, width, height);
   const pixels = imgData.data;
 
+  let hasMore = false;
   colorMap.forEach((arr) => {
     arr.forEach((point) => {
-      const t = interpolate(point.frame / point.duration);
+      const t = interpolate(point.currentFrameIdx / point.totalNumFrames);
 
-      if (currentStageIsImage && point.frame < point.duration) {
-        point.frame++;
+      if (currentStageIsImage && point.currentFrameIdx < point.totalNumFrames) {
+        point.currentFrameIdx++;
         hasMore = true;
-      } else if (!currentStageIsImage && point.frame > 0) {
-        point.frame--;
+      } else if (!currentStageIsImage && point.currentFrameIdx > 0) {
+        point.currentFrameIdx--;
         hasMore = true;
-        if (point.frame > 0) point.frame--;
+        if (point.currentFrameIdx > 0) point.currentFrameIdx--;
       }
 
       const x = Math.round(lerp(point.x, point.destX, t));
@@ -214,7 +224,6 @@ function draw() {
     //   aniReq3 = requestAnimationFrame(draw);
     // }, 1000);
   }
-
 }
 
 function interpolate(t) {
@@ -223,4 +232,45 @@ function interpolate(t) {
 
 function lerp(a, b, t) {
   return b * t + a * (1 - t);
+}
+
+
+/* ----- UI Related ----- */
+
+// for transition to show play/pause button on firstStart
+function showPlayButton() {
+  document.querySelector(".top-menu").classList.add("show-play");
+}
+
+// image info displays
+const progBarTotal = document.getElementById("prog-bar-total");
+const imgDimensions = document.querySelector(".image-dimensions");
+const imgTotalPixels = document.querySelector(".image-total-pixels");
+
+function addImgDimensionsToUI(imgWidth, imgHeight) {
+  const totalNumPixels = formatNumber(imgWidth * imgHeight);
+  progBarTotal.textContent = totalNumPixels;
+  imgDimensions.textContent = `${formatNumber(imgWidth)}px by ${formatNumber(imgHeight)}px`;
+  imgTotalPixels.textContent = `${totalNumPixels} pixels total`;
+}
+
+function formatNumber(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// fake animation for prog bar before implementing gl
+const progBarProcessed = document.getElementById("prog-bar-processed");
+const progBarFilled = document.querySelector(".progress-bar-filled");
+
+function animateProgBar(totalPixels) {
+  let idx = 1;
+  const constantInt = 60;
+  const intId = setInterval(function () {
+    progBarProcessed.textContent = formatNumber(
+      Math.round((idx * totalPixels) / constantInt)
+    );
+    progBarFilled.style.width = idx / constantInt * 100 + "%";
+    idx++;
+    if (idx === constantInt + 1) clearInterval(intId);
+  }, 1000 / constantInt);
 }
