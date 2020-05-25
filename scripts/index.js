801d0b41acd4a052b69268e9b2e4823d545a1bb4
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-25 00:36:33 -0700

    Readjust size of canvas section height on mobile

--- scripts/index.js
+++ scripts/index.js
@@ -453,7 +453,7 @@ function onImageLoad() {
     }
   } else {
     widthAvailable = window.innerWidth;
-    heightAvailable = window.innerHeight - 160;
+    heightAvailable = window.innerHeight - 200;
 
     if (widthAvailable < width || heightAvailable < height) {
       if (widthAvailable < width && heightAvailable < height) {
